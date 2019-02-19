4d3a21c5059454709ea68a1aea1556fa03c43111
ArelaVka <arelav90@mail.ru>
2019-02-19 23:29:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import requests
+import os
+
+directory = 'images'
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 filename = 'img1.jpeg'
+
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
 response = requests.get(url)
