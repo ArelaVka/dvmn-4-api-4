d3f285b603486315ae0c2acc268c6f84ce9340ed
ArelaVka <arelav90@mail.ru>
2019-02-19 23:23:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
-filename = 'HST-SM4.jpeg'
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/'
+filename = 'img1.jpeg'
+url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
 response = requests.get(url)
 with open(filename, 'wb') as file:
