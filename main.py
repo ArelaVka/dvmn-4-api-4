8f05cc8cd264b89f454c9cf1585e370011efb7cb
ArelaVka <arelav90@mail.ru>
2019-02-19 23:48:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
 import os
 
-directory = 'images'
+def make_dir(name_of_dir):
+    if not os.path.exists(name_of_dir):
+        os.makedirs(name_of_dir)
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+def download_img(url, directory):
+    filename = '{}/img1.jpeg'.format(directory)
+    response = requests.get(url)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
-filename = '{}/img1.jpeg'.format(directory)
+if __name__ == "__main__":
+    directory = 'images'
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    make_dir(directory)
+    download_img(url, directory)
+    
 
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
-response = requests.get(url)
-with open(filename, 'wb') as file:
-    file.write(response.content)
