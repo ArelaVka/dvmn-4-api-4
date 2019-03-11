7ae73da24c91105891aa2acd9c383dbebed05d9d
ArelaVka <arelav90@mail.ru>
2019-03-11 23:59:01 +0300

    add download hubble img

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ def fetch_spacex_last_launch():
     with open(filename, 'wb') as file:
         file.write(requests.get(img_link).content)
   
-def get_hubble_links(image_id):
-  #img_links = []
+def get_hubble_img_link(image_id):
   url = 'http://hubblesite.org/api/v3/image/' + image_id
   response_json = requests.get(url).json()
   for num, link in enumerate(response_json["image_files"]):
@@ -24,12 +23,23 @@ def get_hubble_links(image_id):
 
 def get_file_extension(image_link):
   ext = image_link.split('.')
-  return ext
+  return ext[-1]
+
+def download_hubble_img(image_id):
+  directory = 'images'
+  if not os.path.exists(directory):
+        os.makedirs(directory)
+  url = get_hubble_img_link(image_id)
+  ext = get_file_extension(url)
+  filename = '{}.{}'.format(image_id, ext)
+  with open(filename, 'wb') as file:
+        file.write(requests.get(url).content)
 
 
 if __name__ == "__main__":
   image_id = '1'
-  image_link = get_hubble_links(image_id)
-  print(image_link)
+  download_hubble_img(image_id)
+  #image_link = get_hubble_img_link(image_id)
+  #print(image_link)
   #print(get_file_extension(image_link))
   #fetch_spacex_last_launch()
